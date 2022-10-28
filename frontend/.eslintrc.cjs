86e74039c64f435a838b088c39127504885352db
pengxincheng <pengxiaocheng180@163.com>
2022-10-28 09:56:09 +0800

    chore: eslint、prettier规则配置

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['plugin:vue/vue3-essential', 'standard'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'comma-dangle': ['error', 'never']
+  }
+}
