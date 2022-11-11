82c81866b367b12c65d0a51a914e0f74dbfcdd10
pengxincheng <pengxiaocheng180@163.com>
2022-11-11 16:26:25 +0800

    feat [backend] : 查询所有角色接口增加分页查询和按条件过滤

--- backend/controllers/roleController.js
+++ backend/controllers/roleController.js
@@ -4,8 +4,36 @@ import dayjs from 'dayjs';
 
 async function queryList(req, res) {
   try {
-    const role = await Role.find();
-    res.send({ statusCode: res.statusCode, data: role, message: '操作成功' });
+    const { pageNo, pageSize, _t, ...conditions } = req.query;
+
+    let filter = {};
+    if (Object.keys(conditions).length) {
+      filter = { $and: [] };
+      for (const key in conditions) {
+        if (Object.hasOwnProperty.call(conditions, key)) {
+          let obj = {};
+          obj[key] = { $regex: new RegExp(conditions[key], 'i') };
+          filter.$and.push(obj);
+        }
+      }
+    }
+
+    const total = await Role.count();
+    const role = await Role.find(filter)
+      .skip(((parseInt(pageNo) || 1) - 1) * pageSize)
+      .limit(parseInt(pageSize) || 10)
+      .sort({ createTime: -1 });
+
+    res.send({
+      statusCode: res.statusCode,
+      data: {
+        result: role,
+        current: +pageNo || 1,
+        size: +pageSize || 10,
+        total: total
+      },
+      message: '操作成功!'
+    });
   } catch (error) {
     throw boom.boomify(error);
   }
