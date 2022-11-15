497b59fad79e7110cc373531ce99b10f77e6d429
pengxincheng <pengxiaocheng180@163.com>
2022-11-15 13:59:58 +0800

    feat [frontend] : 查询所有角色接口

--- frontend/src/api/modules/role.js
+++ frontend/src/api/modules/role.js
@@ -1,8 +1,9 @@
 import { getAction, deleteAction, postAction, putAction } from '../http'
 
+const queryAllRoleList = (params) => getAction('/role/queryAll', params)
 const queryRoleList = (params) => getAction('/role/queryList', params)
 const deleteRoleById = (params) => deleteAction('/role/deleteById', params)
 const addRole = (params) => postAction('/role/add', params)
 const editRole = (params) => putAction('/role/edit', params)
 
-export { queryRoleList, deleteRoleById, addRole, editRole }
+export { queryAllRoleList, queryRoleList, deleteRoleById, addRole, editRole }
