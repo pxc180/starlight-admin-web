545d5ce7ece537a0ab3991d0d6cb6d69dca17aac
pengxincheng <pengxiaocheng180@163.com>
2022-12-26 16:25:55 +0800

    feat [backend] : Fastify实例增加onRequest Hook用来解析token信息

--- backend/fastify.js
+++ backend/fastify.js
@@ -3,6 +3,7 @@ import fastifySwagger from '@fastify/swagger';
 import fastifyUi from '@fastify/swagger-ui';
 import fastifyJwt from '@fastify/jwt';
 import { options as swagger } from './config/swagger.js';
+import boom from '@hapi/boom';
 
 const fastify = new Fastify({
   logger: { level: 'info' }
@@ -12,5 +13,13 @@ const fastify = new Fastify({
 await fastify.register(fastifySwagger);
 await fastify.register(fastifyUi, swagger);
 await fastify.register(fastifyJwt, { secret: 'supersecret' });
+
+fastify.addHook('onRequest', async (req, reply) => {
+  try {
+    await req.jwtVerify();
+  } catch (error) {
+    throw boom.boomify(error);
+  }
+});
 
 export default fastify;
